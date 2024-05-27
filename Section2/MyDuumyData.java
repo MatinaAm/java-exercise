a482176f54ddf62f5ad2f7202bde4557d95a2504
m.amannejad <m.amannejad@fanap.ir>
2024-05-28 00:07:21 +0330

    prettier

--- Section2/MyDuumyData.java
+++ Section2/MyDuumyData.java
@@ -6,29 +6,20 @@ import java.util.List;
 
 public class MyDuumyData {
 
-
     public static void main(String[] args) {
-
         List<Person> persons = new ArrayList<>();
 
         try {
-            persons.add(new Person(1, "متینا امان نژاد", 30, "زن", Person.stringToDate("1372/07/18"), "matina.amani@gmail.com" , Long.MAX_VALUE, true));
-
+            persons.add(new Person(1, "متینا امان نژاد", 30, "زن", Person.stringToDate("1372/07/18"), "matina.amani@gmail.com", Long.MAX_VALUE, true));
         } catch (ParseException e) {
             e.printStackTrace();
         }
 
         // نمایش داده‌ها
         for (Person person : persons) {
-            System.out.println("my dummy data is :"+person);
+            System.out.println("my dummy data is :" + person);
         }
-
-
-
-
     }
-
-
 
 
 }
