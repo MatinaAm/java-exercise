1fa05c9f8fb3273041e8107931022c5281432ced
m.amannejad <m.amannejad@fanap.ir>
2024-06-18 01:00:33 +0330

    section 4

---
+++ Section4/App.java
@@ -0,0 +1,20 @@
+package Section4;
+
+public class App {
+
+    public static void main(String[] args) {
+        int number = 7;
+        int result = factorialFunc(number);
+        System.out.println("value of factorial function with input " + number + " is: " + result);
+    }
+        public static int factorialFunc(int x) {
+            if (x <= 1) {
+                return 1;
+            } else {
+                return x * factorialFunc(x- 1);
+            }
+        }
+
+    }
+
+
